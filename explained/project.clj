81fe7775c9442c4a4897c6da1a425e21f34d3d14
Tom Bombabil <amanibhavam@destructuring.org>
2015-11-05 23:45:06 -0800

    remove description from project, for now

--- explained/project.clj
+++ explained/project.clj
@@ -1,9 +1,4 @@
 (defproject explained "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [ring-server "0.4.0"]
                  [reagent "0.5.1"]
