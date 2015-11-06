3af30968e275fc3f14fe57bba784d1b24fd36b85
Tom Bombabil <amanibhavam@destructuring.org>
2015-11-05 23:45:37 -0800

    indented with vim =

--- explained/project.clj
+++ explained/project.clj
@@ -1,98 +1,98 @@
 (defproject explained "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [ring-server "0.4.0"]
-                 [reagent "0.5.1"]
-                 [reagent-forms "0.5.13"]
-                 [reagent-utils "0.1.5"]
-                 [ring "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]
-                 [prone "0.8.2"]
-                 [compojure "1.4.0"]
-                 [hiccup "1.0.5"]
-                 [environ "1.0.1"]
-                 [org.clojure/clojurescript "1.7.145" :scope "provided"]
-                 [secretary "1.2.3"]
-                 [venantius/accountant "0.1.4"]
-                  [devcards "0.2.0-8"] 
-                 ]
+            :dependencies [[org.clojure/clojure "1.7.0"]
+                           [ring-server "0.4.0"]
+                           [reagent "0.5.1"]
+                           [reagent-forms "0.5.13"]
+                           [reagent-utils "0.1.5"]
+                           [ring "1.4.0"]
+                           [ring/ring-defaults "0.1.5"]
+                           [prone "0.8.2"]
+                           [compojure "1.4.0"]
+                           [hiccup "1.0.5"]
+                           [environ "1.0.1"]
+                           [org.clojure/clojurescript "1.7.145" :scope "provided"]
+                           [secretary "1.2.3"]
+                           [venantius/accountant "0.1.4"]
+                           [devcards "0.2.0-8"] 
+                           ]
 
-  :plugins [[lein-environ "1.0.1"]
-            [lein-asset-minifier "0.2.2"]]
+            :plugins [[lein-environ "1.0.1"]
+                      [lein-asset-minifier "0.2.2"]]
 
-  :ring {:handler explained.handler/app
-         :uberwar-name "explained.war"}
+            :ring {:handler explained.handler/app
+                   :uberwar-name "explained.war"}
 
-  :min-lein-version "2.5.0"
+            :min-lein-version "2.5.0"
 
-  :uberjar-name "explained.jar"
+            :uberjar-name "explained.jar"
 
-  :main explained.server
+            :main explained.server
 
-  :clean-targets ^{:protect false} [:target-path
-                                    [:cljsbuild :builds :app :compiler :output-dir]
-                                    [:cljsbuild :builds :app :compiler :output-to]]
+            :clean-targets ^{:protect false} [:target-path
+                                              [:cljsbuild :builds :app :compiler :output-dir]
+                                              [:cljsbuild :builds :app :compiler :output-to]]
 
-  :source-paths ["src/clj" "src/cljc"]
+            :source-paths ["src/clj" "src/cljc"]
 
-  :minify-assets
-  {:assets
-    {"resources/public/css/site.min.css" "resources/public/css/site.css"}}
+            :minify-assets
+            {:assets
+             {"resources/public/css/site.min.css" "resources/public/css/site.css"}}
 
-  :cljsbuild {:builds {:app {:source-paths ["src/cljs" "src/cljc"]
-                             :compiler {:output-to     "resources/public/js/app.js"
-                                        :output-dir    "resources/public/js/out"
-                                        :asset-path   "js/out"
-                                        :optimizations :none
-                                        :pretty-print  true}}}}
+            :cljsbuild {:builds {:app {:source-paths ["src/cljs" "src/cljc"]
+                                       :compiler {:output-to     "resources/public/js/app.js"
+                                                  :output-dir    "resources/public/js/out"
+                                                  :asset-path   "js/out"
+                                                  :optimizations :none
+                                                  :pretty-print  true}}}}
 
-  :profiles {:dev {:repl-options {:init-ns explained.repl}
+            :profiles {:dev {:repl-options {:init-ns explained.repl}
 
-                   :dependencies [[ring/ring-mock "0.3.0"]
-                                  [ring/ring-devel "1.4.0"]
-                                  [lein-figwheel "0.4.1"]
-                                  [org.clojure/tools.nrepl "0.2.11"]
-                                  [com.cemerick/piggieback "0.1.5"]
-                                  [pjstadig/humane-test-output "0.7.0"]]
+                             :dependencies [[ring/ring-mock "0.3.0"]
+                                            [ring/ring-devel "1.4.0"]
+                                            [lein-figwheel "0.4.1"]
+                                            [org.clojure/tools.nrepl "0.2.11"]
+                                            [com.cemerick/piggieback "0.1.5"]
+                                            [pjstadig/humane-test-output "0.7.0"]]
 
-                   :source-paths ["env/dev/clj"]
-                   :plugins [[lein-figwheel "0.4.1"]
-                             [lein-cljsbuild "1.1.0"]]
+                             :source-paths ["env/dev/clj"]
+                             :plugins [[lein-figwheel "0.4.1"]
+                                       [lein-cljsbuild "1.1.0"]]
 
-                   :injections [(require 'pjstadig.humane-test-output)
-                                (pjstadig.humane-test-output/activate!)]
+                             :injections [(require 'pjstadig.humane-test-output)
+                                          (pjstadig.humane-test-output/activate!)]
 
-                   :figwheel {:http-server-root "public"
-                              :server-port 3449
-                              :nrepl-port 7002
-                              :nrepl-middleware ["cemerick.piggieback/wrap-cljs-repl"
-                                                 ]
-                              :css-dirs ["resources/public/css"]
-                              :ring-handler explained.handler/app}
+                             :figwheel {:http-server-root "public"
+                                        :server-port 3449
+                                        :nrepl-port 7002
+                                        :nrepl-middleware ["cemerick.piggieback/wrap-cljs-repl"
+                                                           ]
+                                        :css-dirs ["resources/public/css"]
+                                        :ring-handler explained.handler/app}
 
-                   :env {:dev true}
+                             :env {:dev true}
 
-                   :cljsbuild {:builds {:app {:source-paths ["env/dev/cljs"]
-                                              :compiler {:main "explained.dev"
-                                                         :source-map true}}
+                             :cljsbuild {:builds {:app {:source-paths ["env/dev/cljs"]
+                                                        :compiler {:main "explained.dev"
+                                                                   :source-map true}}
 
-                                        :devcards {:source-paths ["src/cljs" "src/cljc" "env/dev/cljs"]
-                                                   :figwheel {:devcards true}
-                                                   :compiler {:main "explained.cards"
-                                                              :asset-path "js/devcards_out"
-                                                              :output-to "resources/public/js/app_devcards.js"
-                                                              :output-dir "resources/public/js/devcards_out"
-                                                              :source-map-timestamp true}}
-                                        }
+                                                  :devcards {:source-paths ["src/cljs" "src/cljc" "env/dev/cljs"]
+                                                             :figwheel {:devcards true}
+                                                             :compiler {:main "explained.cards"
+                                                                        :asset-path "js/devcards_out"
+                                                                        :output-to "resources/public/js/app_devcards.js"
+                                                                        :output-dir "resources/public/js/devcards_out"
+                                                                        :source-map-timestamp true}}
+                                                  }
 
-                               }}
+                                         }}
 
-             :uberjar {:hooks [leiningen.cljsbuild minify-assets.plugin/hooks]
-                       :env {:production true}
-                       :aot :all
-                       :omit-source true
-                       :cljsbuild {:jar true
-                                   :builds {:app
-                                             {:source-paths ["env/prod/cljs"]
-                                              :compiler
-                                              {:optimizations :advanced
-                                               :pretty-print false}}}}}})
+                       :uberjar {:hooks [leiningen.cljsbuild minify-assets.plugin/hooks]
+                                 :env {:production true}
+                                 :aot :all
+                                 :omit-source true
+                                 :cljsbuild {:jar true
+                                             :builds {:app
+                                                      {:source-paths ["env/prod/cljs"]
+                                                       :compiler
+                                                       {:optimizations :advanced
+                                                        :pretty-print false}}}}}})
