cee58bc708bd1c512652bc0ba312520cf1d9d4ce
Tom Bombabil <amanibhavam@destructuring.org>
2015-11-05 23:38:44 -0800

    scaffolding from lein new reagent

---
+++ explained/src/clj/explained/server.clj
@@ -0,0 +1,9 @@
+(ns explained.server
+  (:require [explained.handler :refer [app]]
+            [environ.core :refer [env]]
+            [ring.adapter.jetty :refer [run-jetty]])
+  (:gen-class))
+
+ (defn -main [& args]
+   (let [port (Integer/parseInt (or (env :port) "3000"))]
+     (run-jetty app {:port port :join? false})))
